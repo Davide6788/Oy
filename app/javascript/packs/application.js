2840417987b69c6f4f729d31b909a6acc0cc7863
victor plantevin <victor.plantevin@gmail.com>
2022-01-29 17:18:30 +0100

    map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
@@ -24,7 +23,31 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+
+const mapElement = document.getElementById("map");
+const markers = JSON.parse(mapElement.dataset.markers);
+
+mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+});
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach((marker) => bounds.extend([marker.lng, marker.lat]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+markers.forEach((marker) => {
+  const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+  new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(map);
+});
+fitMapToMarkers(map, markers);
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_191049) do
+ActiveRecord::Schema.define(version: 2022_01_29_141655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2022_01_27_191049) do
     t.bigint "reward_mechanism_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["reward_mechanism_id"], name: "index_businesses_on_reward_mechanism_id"
     t.index ["user_id"], name: "index_businesses_on_user_id"
   end
