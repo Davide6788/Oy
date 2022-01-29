a13bc944e971ebdbccd29fc2f3101e85cbea4885
davide osella <oselladavide@gmail.com>
2022-01-29 15:03:59 +0100

    added reference from business to category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_140022) do
+ActiveRecord::Schema.define(version: 2022_01_29_140323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_01_29_140022) do
     t.bigint "reward_mechanism_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_businesses_on_category_id"
     t.index ["reward_mechanism_id"], name: "index_businesses_on_reward_mechanism_id"
     t.index ["user_id"], name: "index_businesses_on_user_id"
   end
@@ -34,6 +36,12 @@ ActiveRecord::Schema.define(version: 2022_01_29_140022) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["business_id"], name: "index_cards_on_business_id"
     t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "reward_mechanisms", force: :cascade do |t|
@@ -56,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_01_29_140022) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "businesses", "categories"
   add_foreign_key "businesses", "reward_mechanisms"
   add_foreign_key "businesses", "users"
   add_foreign_key "cards", "businesses"
