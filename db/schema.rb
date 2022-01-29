88eac366766020ae8a9da5322a8df3d087666c10
davide osella <oselladavide@gmail.com>
2022-01-29 17:40:32 +0100

    updated schema on master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_01_29_141655) do
 
   # These are extensions that must be enabled in order to support this database
@@ -23,9 +22,9 @@ ActiveRecord::Schema.define(version: 2022_01_29_141655) do
     t.bigint "reward_mechanism_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
     t.float "latitude"
     t.float "longitude"
-    t.bigint "category_id", null: false
     t.index ["category_id"], name: "index_businesses_on_category_id"
     t.index ["reward_mechanism_id"], name: "index_businesses_on_reward_mechanism_id"
     t.index ["user_id"], name: "index_businesses_on_user_id"
