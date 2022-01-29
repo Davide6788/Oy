01f8af05a9193b6da2d627d92edc1f9c253d96a6
victor plantevin <victor.plantevin@gmail.com>
2022-01-29 12:30:01 +0100

    business controller

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,2 +1,30 @@
 class BusinessesController < ApplicationController
+  def new
+    @business = Business.find(params[:id])
+  end
+
+  def create
+    @business = Business.new(business_params)
+    if @business.save
+      redirect_to business_path(@business)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @business = Business.find(params[:id])
+    @business.destroy
+  end
+
+  def update
+    @business = Business.find(params[:id])
+    @business.update(params[:business])
+  end
+
+  private
+
+  def business_params
+    params.require(:business).permit(:name, :address, :category)
+  end
 end
