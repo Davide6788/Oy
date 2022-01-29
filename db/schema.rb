28f016e1571c4fe94535d71bb3d2d4c73a905115
davide osella <oselladavide@gmail.com>
2022-01-29 15:01:21 +0100

    removed category column from businesses model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_191049) do
+ActiveRecord::Schema.define(version: 2022_01_29_140022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2022_01_27_191049) do
   create_table "businesses", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.string "category"
     t.bigint "user_id", null: false
     t.bigint "reward_mechanism_id", null: false
     t.datetime "created_at", precision: 6, null: false
