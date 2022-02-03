d3eab9e333d8ac9fd9beb8505105540d4a80f1c9
Hannah Broucaret <broucarethannah@gmail.com>
2022-02-03 21:43:57 +0100

    started dropdown - front to fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,23 +31,26 @@ document.addEventListener("turbolinks:load", () => {
 import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 
 const mapElement = document.getElementById("map");
-const markers = JSON.parse(mapElement.dataset.markers);
 
-mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-const map = new mapboxgl.Map({
-  container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
-});
+if (mapElement) {
+  const markers = JSON.parse(mapElement.dataset.markers);
 
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach((marker) => bounds.extend([marker.lng, marker.lat]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-};
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  const map = new mapboxgl.Map({
+    container: "map", // container ID
+    style: "mapbox://styles/mapbox/streets-v11", // style URL
+  });
 
-markers.forEach((marker) => {
-  const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+  const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach((marker) => bounds.extend([marker.lng, marker.lat]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  };
 
-  new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(map);
-});
-fitMapToMarkers(map, markers);
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+    new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(map);
+  });
+  fitMapToMarkers(map, markers);
+}
