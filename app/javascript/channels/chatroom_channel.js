93ca495fade3d16864924a5afb81395530e2f844
Matt Clark <clarkm02@gmail.com>
2022-02-19 15:24:50 +0100

    making changes, but it's not working

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -9,7 +9,7 @@ const initChatroomCable = () => {
       { channel: "ChatroomChannel", id: id },
       {
         received(data) {
-          postsContainer.insertAdjacentHTML("beforebegin", data);
+          postsContainer.insertAdjacentHTML("afterbegin", data);
         },
       }
     );
