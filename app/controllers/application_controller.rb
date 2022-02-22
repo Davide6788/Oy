00f0f6fc38859fd985928d960ded79f39bf5bc79
victor plantevin <victor.plantevin@gmail.com>
2022-02-22 20:04:52 +0100

    metatag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,18 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
 
   add_flash_types :notice
-  ##include Pundit
+  # #include Pundit
 
-  ##after_action :verify_authorized, except: [:index], unless: :skip_pundit?
-  ##after_action :verify_policy_scoped, only: [:index], unless: :skip_pundit?
+  # #after_action :verify_authorized, except: [:index], unless: :skip_pundit?
+  # #after_action :verify_policy_scoped, only: [:index], unless: :skip_pundit?
 
   # private
 
   # def skip_pundit?
   #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   # end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
